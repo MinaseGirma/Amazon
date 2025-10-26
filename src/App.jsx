a3f4748fa7152937d4716314c28849ea2f9aea28
minase <minasegirma5@gmail.com>
2025-10-26 14:57:57 +0100

    header part

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Component/Header/Header.jsx'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />  
+      <Header /> 
       
       </>
   )
--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,62 +1,75 @@
-import React from 'react'
-import { BsSearch } from 'react-icons/bs';
-import { BiCart } from 'react-icons/bi';
-import { SlLocationPin } from 'react-icons/sl';
+import { BsSearch } from "react-icons/bs"
+import { BiCart } from "react-icons/bi"
+import { SlLocationPin } from "react-icons/sl"
+import styles from "./header.module.css"
+import Headerbottom from "./Headerbottom.jsx"
 
 const Header = () => {
   return (
-    <section>
-        <section>
-            <div>
-                {/* logo */}
-                <a href="">
-                    <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" /></a>
-                {/* {deli veryLocation} */}
-                <span>{<SlLocationPin />}</span>
-                <div>
-                    <p>Delivered to</p>
-                    <span>Ethiopia</span>
-                </div>
+    <>
+    <section className={styles.header}>
+      <section className={styles.headerContainer}>
+        <div className={styles.headerLeft}>
+          {/* logo */}
+          <a href="" className={styles.logo}>
+            <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" />
+          </a>
+          {/* delivery location */}
+          <div className={styles.location}>
+            <span className={styles.locationIcon}>
+              <SlLocationPin />
+            </span>
+            <div className={styles.locationText}>
+              <p>Deliver to</p>
+              <span>Ethiopia</span>
             </div>
-            <div><select name="" id="">
-                <option value="">All</option></select> 
-                <input type="text" name="" id="" placeholder="Search products" />
-                {/* icon */}
-                <span>{<BsSearch />}</span>
-                </div>
-            {/* right side */}
-            <div>
-                    <img src="" alt="" />
-                    <section>
-                        <option value="">EN</option>
-                    </section>
-            </div>
-            <div>
-                {/* three components */}
-                <a href="">
-                    <div>
-                        <p>Sign In</p>
-                        <span>Account & Lists</span>
-                    </div>
-                </a>
-                {/* orders */}
-                <a href="">
-                    <div>
-                        <p>Returns</p>
-                        <span>& Orders</span>
-                    </div>
-                </a>
-                {/* cart */}
-               <a to={"cart"}>
-                   <div>
-                       {/* icon */}
-                       <span>{<BiCart />}</span>
-                       <span>Cart</span>
-                   </div>
-               </a>
-            </div>
-        </section>
+          </div>
+        </div>
+
+        <div className={styles.headerSearch}>
+          <select name="" id="">
+            <option value="">All</option>
+          </select>
+          <input type="text" name="" id="" placeholder="Search Amazon" />
+          {/* search icon */}
+          <span className={styles.searchIcon}>
+            <BsSearch />
+          </span>
+        </div>
+
+        <div className={styles.headerRight}>
+          {/* language selector */}
+          <div className={styles.language}>
+            <img src="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg" alt="US flag" />
+            <select name="" id="">
+              <option value="">EN</option>
+            </select>
+          </div>
+
+          {/* sign in */}
+          <a href="" className={styles.headerLink}>
+            <p>Hello, Sign in</p>
+            <span>Account & Lists</span>
+          </a>
+
+          {/* orders */}
+          <a href="" className={styles.headerLink}>
+            <p>Returns</p>
+            <span>& Orders</span>
+          </a>
+
+          {/* cart */}
+          <a href="/cart" className={styles.cart}>
+            <span className={styles.cartIcon}>
+              <BiCart />
+            </span>
+            <span>Cart</span>
+          </a>
+        </div>
+      </section>
     </section>
+    <Headerbottom />
+    </>
   )
 }
 

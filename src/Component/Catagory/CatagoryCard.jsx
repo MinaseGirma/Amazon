de2c7370f83370124021ec8bb645568a1cdd3505
minase <minasegirma5@gmail.com>
2025-10-26 20:53:47 +0100

    catagory

---
+++ src/Component/Catagory/CatagoryCard.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import classes from "./catagory.module.css"
+
+function CatagoryCard({ data }) {
+  return (
+    <div className={classes.catagory}>
+      <a href="">
+        <span>
+          <h2>{data.title}</h2>
+        </span>
+        <img src={data.img || "/placeholder.svg"} alt="" />
+        <p>shop now</p>
+      </a>
+    </div>
+  )
+}
+
+export default CatagoryCard
---
+++ src/Component/Catagory/Catagoryfullinfo.js
@@ -0,0 +1,26 @@
+export const catagoryInfo = [
+  {
+    title: "Electronics",
+    name: "electronics",
+    img: "https://images.unsplash.com/photo-1498049794561-7780e7231661?w=400&h=400&fit=crop",
+  },
+  {
+    title: "Fashion",
+    name: "fashion",
+    img: "https://images.unsplash.com/photo-1445205170230-053b83016050?w=400&h=400&fit=crop",
+  },
+  {
+    title: "Books",
+    name: "books",
+    img: "https://images.unsplash.com/photo-1495446815901-a7297e633e8d?w=400&h=400&fit=crop",
+  },
+  {
+    title: "Home & Kitchen",
+    name: "home-kitchen",
+    img: "https://images.unsplash.com/photo-1556911220-bff31c812dba?w=400&h=400&fit=crop",
+  },
+  
+  
+]
+
+export default catagoryInfo
